d84626dcae44caa8aa350f35a58e80aac4d26c21
Darsh Mittal <80146115+thatsdarsh@users.noreply.github.com>
2021-08-05 16:58:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+ var name_of_the_student_array = [];
+
+function submit() {
+    var name_1 = document.getElementById("#no1").value;
+    var name_2 = document.getElementById("#no2").value;
+    var name_3 = document.getElementById("#no3").value;
+    var name_4 = document.getElementById("#no4").value;
+
+    name_of_the_student_array.push(name_1);
+    name_of_the_student_array.push(name_2);
+    name_of_the_student_array.push(name_3);
+    name_of_the_student_array.push(name_4);
+
+    console.log(name_of_the_student_array);
+
+    document.getElementById("display_name").innerHTML = name_of_the_student_array;
+    document.getElementById("submit_button").style.display = "none";
+    document.getElementById("sort_button").style.display = "inline-block";
+}
+
+function sorting() {
+    name_of_the_student_array.sort();
+    console.log(name_of_the_student_array);
+    document.getElementById("display_name").innerHTML = name_of_the_student_array;
+}
